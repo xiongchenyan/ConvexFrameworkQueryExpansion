648aec0538a313a440e7d47a4f7ae2e03af71935
chenyanxiong <xiongchenyan@gmail.com>
2014-05-29 14:56:21 -0400

    use matlab kmeans

--- ObjectClustering/ObjectKmeans.py
+++ ObjectClustering/ObjectKmeans.py
@@ -14,42 +14,61 @@ class ObjectKMeansC(cxBaseC):
         self.InName = ""
         self.OutName = ""
         self.k = 8
+        self.MidDir = ""
     
     @staticmethod
     def ShowConf():
-        print "in\nout\nk"
+        print "in\nout\nk\nmiddir"
     
     def SetConf(self,ConfIn):
         conf = cxConf(ConfIn)
         self.InName = conf.GetConf('in')
         self.OutName = conf.GetConf('out')
         self.k = int(conf.GetConf('k',self.k))
-        self.Model = KMeans(np_cluster = self.k)
+        self.MidDir = conf.GetConf('middir')
+        self.Model = cxKMeansC()
+        self.Model.k = self.k
+        self.Model.workdir = self.MidDir
+        
     
     
-    def SegDataFromLine(self,line):
-        vCol = line.strip().split('\t')
-        if len(vCol) < 5:
-            return ""
-        Vector = VectorC()
-        Vector.loads(vCol[4])
-        l = Vector.hDim.items()
-        l.sort(key=lambda item:item[0])
-        res = [item[1] for item in l]
-        return res
+    def CreateSpMtx(self,lLines):
+        data = []
+        hFeatureIndex = {}
+        
+        DataIndex= 1
+        for line in lLines:
+            vCol = line.strip().split('\t')
+            hFeature = json.loads(vCol[4])
+            for Feature in hFeature:
+                FeatureP = len(hFeatureIndex) + 1
+                if Feature in hFeatureIndex:
+                    FeatureP = hFeatureIndex[Feature]
+                else:
+                    hFeatureIndex[Feature] = FeatureP
+                data.append([DataIndex,FeatureP,hFeature[Feature]])
+            DataIndex += 1
+        return data 
+            
         
         
     def ProcessOneQ(self,lLines):
-        lData = []
-        for line in lLines:
-            data = self.SegDataFromLine(line)
-            if "" != data:
-                lData.append(data)
-        lData = np.array(lData)
-        lLabel = list(self.Model.fit_predict(lData))
-        
+        data = self.CreateSpMtx(lLines)
+        lLabel = self.Model.ProcessData(data)
         return lLabel
     
+    
+    def DiscardBadLine(self,lLines):
+        lRes = []
+        for line in lLines:
+            if '{}' in line:
+                continue
+            vCol = line.strip().split('\t')
+            if len(vCol) < 5:
+                continue
+            lRes.append(line)
+        return lRes
+        
     
     def Process(self):
         
@@ -59,13 +78,12 @@ class ObjectKMeansC(cxBaseC):
         out = open(self.OutName,'w')
         
         for lLines in Reader:
+            lLines = self.DiscardBadLine(lLines)
             lLabel = self.ProcessOneQ(lLines)
             cnt = 0
             for i in range(len(lLines)):
                 line = lLines[i]
                 vCol = line.strip().split('\t')
-                if len(vCol) < 5:
-                    continue
                 vCol[4] = str(lLabel[cnt])
                 cnt += 1
                 print >> out,'\t'.join(vCol)
